cb9a0b656b2afd2e2bfcfe2ff1814ef517acda7c
Longs Pemun Gotar <developerlongs@gmail.com>
2022-10-28 10:38:32 +0000

    changes

--- deck.go
+++ deck.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+	"io/ioutil"
 	"fmt"
 	"strings"
 )
@@ -39,4 +41,15 @@ func (d deck) toString() string {
 	return strings.Join([]string(d), ",") 
 }
 
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
 
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil{
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+}
